1918174faf36b9e40121b781f5f146f46c9ebd6b
Magnaldo Melo <magnaldo.melo@gmail.com>
2020-03-14 15:32:13 -0300

    Package de conexão

--- db/connect.go
+++ db/connect.go
@@ -8,7 +8,11 @@ import (
 
 func conectar() *sql.DB {
 	fmt.Println("Go com mysql")
-	db, _ := sql.Open("mysql", "root:foxtro2008@tcp(172.17.0.1:3306)/golang")
+	db, err := sql.Open("mysql", "root:foxtro2008@tcp(172.17.0.1:3306)/golang")
+
+	if err != nil {
+		fmt.Println(err.Error())
+	}
 
 	fmt.Println("Banco conectado com sucesso!")
 	return db
